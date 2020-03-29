63234de2b832c99452ecf2578ab7622d80bb8fab
SaleLanding <62724413+SaleLanding@users.noreply.github.com>
2020-03-29 14:41:01 +0300

    form305

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
 			url: "mail.php", //Change
 			data: th.serialize()
 		}).done(function() {
-			alert("Thank you !!");
+			alert("Thank you !");
 			setTimeout(function() {
 				// Done Functions
 				th.trigger("reset");
